dadffe58ca382f5058b3cc695c9a88169431483e
Chaudhary Sumit <86030135+chaudharysumit001@users.noreply.github.com>
2022-09-07 05:54:13 +0530

    Time: 4 ms (22.58%), Space: 5.9 MB (57.14%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,14 +1,15 @@
 class Solution {
  public:
   int climbStairs(int n) {
-    // dp[i] := # of distinct ways to climb to i-th stair
-    vector<int> dp(n + 1);
-    dp[0] = 1;
-    dp[1] = 1;
+    int prev1 = 1;  // dp[i - 1]
+    int prev2 = 1;  // dp[i - 2]
 
-    for (int i = 2; i <= n; ++i)
-      dp[i] = dp[i - 1] + dp[i - 2];
+    for (int i = 2; i <= n; ++i) {
+      const int dp = prev1 + prev2;
+      prev2 = prev1;
+      prev1 = dp;
+    }
 
-    return dp[n];
+    return prev1;
   }
 };
