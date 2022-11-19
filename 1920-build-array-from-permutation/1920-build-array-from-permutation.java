9fe2038cc7dce79c35e44230dca13491eab11843
Chaudhary Sumit <86030135+chaudharysumit001@users.noreply.github.com>
2022-11-19 10:46:20 +0530

    Time: 9 ms (5.40%), Space: 54.1 MB (11.00%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.java
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.java
@@ -2,6 +2,7 @@ class Solution {
     public int[] buildArray(int[] nums) {
         int n = nums.length;
         for(int i = 0; i < n ; i++){
+            // main line for reducing complexcity kaise aai pata nai
         nums[i] = n*(nums[nums[i]]%n)+ nums[i];
         }
         for(int i = 0; i < n ; i++){
