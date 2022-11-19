bc5f8e300385b1eca7f47d7c7d1e9be7900750bb
Chaudhary Sumit <86030135+chaudharysumit001@users.noreply.github.com>
2022-11-19 06:11:58 +0530

    Time: 2 ms (58.02%), Space: 42.1 MB (23.49%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -3,37 +3,30 @@ class Solution {
         int start = 0;
         int end = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-            start = Math.max(start, nums[i]); // in the end of the loop this will contain the max item of the array
+        for (int i = 0; i < nums.length; i++){
+            start = Math.max(start, nums[i]);
             end += nums[i];
         }
 
-        // binary search
-        while (start < end) {
-            // try for the middle as potential ans
-            int mid = start + (end - start) / 2;
-
-            // calculate how many pieces you can divide this in with this max sum
+        while (start < end){
+            int mid = start + (end - start)/2;
             int sum = 0;
             int pieces = 1;
-            for(int num : nums) {
+            for (int num : nums) {
                 if (sum + num > mid) {
-                    // you cannot add this in this subarray, make new one
-                    // say you add this num in new subarray, then sum = num
                     sum = num;
                     pieces++;
                 } else {
                     sum += num;
                 }
             }
-
             if (pieces > m) {
-                start = mid + 1;
-            } else {
+                start = mid+1;
+            }else{
                 end = mid;
             }
 
         }
-        return end; // here start == end
+        return end;
     }
 }
