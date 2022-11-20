0281bac461bc560722287f479f25225e8ddc1561
Chaudhary Sumit <86030135+chaudharysumit001@users.noreply.github.com>
2022-11-20 12:32:42 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (96.87%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.java
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int numIdenticalPairs(int[] nums) {
         int res = 0;
         for(int i = 0; i < nums.length; i++){
@@ -8,6 +8,18 @@ class Solution {
                 }
             }
         }
+        return res;
+    }
+}*/
+class Solution{
+    public int numIdenticalPairs(int[] nums) {
+        int temp [] = new int[101];
+        int res = 0;
+        for(int i=0; i < nums.length; i++){
+            res += temp[nums[i]];
+            temp[nums[i]]++;
+        }
+        
         return res;
     }
 }
