3c53885c5520d23427bca78281c1be4691c6de1e
Chaudhary Sumit <86030135+chaudharysumit001@users.noreply.github.com>
2023-01-05 20:57:57 +0530

    Time: 1 ms (38.24%), Space: 42.5 MB (26.67%) - LeetHub

---
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] sumZero(int n) {
+        int []arr = new int[n];
+        if(n%2 == 0){
+            for(int i =1; i < n; i=i+2){
+                arr[i-1] = i;
+                arr[i] = -i;
+            }
+        }
+        else{
+            arr[n-1] = 0;
+            for(int i = 1; i < n-1; i= i+2){
+                arr[i-1] = i;
+                arr[i] = -i;
+            }
+        }
+        return arr;
+    }
+}
