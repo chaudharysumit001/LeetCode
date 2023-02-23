3cf3d2203d365fcf665cc6e9d11acc2649ea4350
Chaudhary Sumit <86030135+chaudharysumit001@users.noreply.github.com>
2023-02-23 07:30:46 +0530

    Time: 45 ms (98.29%), Space: 96.4 MB (12.14%) - LeetHub

---
+++ 0502-ipo/0502-ipo.java
@@ -0,0 +1,63 @@
+class Solution {
+    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+
+        var maxHeap = new PriorityQueue<Integer>(Comparator.reverseOrder());
+        Quick.sort(profits, capital, 0, profits.length - 1);
+
+        if (capital[0] > w) return 0;
+        int l = 0;
+        int oldl = -1;
+        // each step find the most
+        while (k > 0) {
+            int r = profits.length - 1;
+            // binary search for efficiency, upper limit
+            while (l < r) {
+                // find mid to avoid loops
+                int m = r - (r - l) / 2;
+                if (capital[m] <= w) {
+                    l = m;
+                } else r = m - 1;
+            }
+            // add all new elements
+            if (oldl != l) {
+                for (int i = oldl + 1; i <= l; i++)
+                    maxHeap.add(profits[i]);
+                oldl = l;
+            }
+            // pop max profit
+            if (maxHeap.isEmpty()) return w;
+            w += maxHeap.poll();
+            k--;
+        }
+        return w;
+    }
+}
+
+class Quick {
+    public static void sort(int[] profits, int[] capital, int l, int r) {
+        if (l < 0 || r < 0 || l >= r) return;
+        var p = partition(profits, capital, l, r);
+        sort(profits, capital, l, p);
+        sort(profits, capital, p + 1, r);
+    }
+
+    public static int partition(int[] profits, int[] capital, int l, int r) {
+        int pv = capital[(l + r) / 2];
+        int pf = profits[(l + r) / 2];
+
+        l -= 1;
+        r += 1;
+        while (true) {
+            do l++; while (capital[l] < pv);
+            do r--; while (capital[r] > pv);
+            if (l >= r) return r;
+            int t = profits[l];
+            profits[l] = profits[r];
+            profits[r] = t;
+
+            t = capital[l];
+            capital[l] = capital[r];
+            capital[r] = t;
+        }
+    }
+}
