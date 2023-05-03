c67aba4c12d067d37aad64e4aec38b7b2912d2a5
Chaudhary Sumit <86030135+chaudharysumit001@users.noreply.github.com>
2023-05-03 17:48:42 +0530

    Time: 23 ms (14.20%), Space: 43 MB (26.34%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.java
+++ 1002-find-common-characters/1002-find-common-characters.java
@@ -19,6 +19,8 @@ class Solution {
             for(char ch = 'a'; ch <= 'z'; ch++){
                 minFrequency.put(ch,Math.min(minFrequency.getOrDefault(ch,0),currFrequency.getOrDefault(ch,0)));
             }   
+            // is line se words m jo bhi h unki minimum mil jygi or jo words given string me nahi h wo 0 initialize ho jaye ab jese example 1 roller baad m aata h to r kahi phle use nai hua to uski minimum frequncy 0 hi rhegi 
+            // main main baat h jo letter first word m use hue h unke hi letter aage check honge baki 0 hi rhenge or koi word unme se b gayab hua to wo bhi 0 kr diya jyga
         }
         
         for(char ch = 'a'; ch <= 'z'; ch++){
