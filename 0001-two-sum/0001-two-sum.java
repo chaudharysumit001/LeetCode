67f494c61816546b9752f3bfb41c96d26a1a0de6
Chaudhary Sumit <86030135+chaudharysumit001@users.noreply.github.com>
2023-07-01 07:54:12 +0530

    Time: 4 ms (63.07%), Space: 43.8 MB (40.23%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,22 +1,40 @@
-class Solution {
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         Map<Integer , Integer> map = new HashMap <>();
+//         for(int i = 0; i<nums.length;  i++){
+//             map.put(nums[i],i);
+//          }
+//         for(int j =0;j<nums.length;j++){
+//             int val = nums[j];
+//             int rem = target - val;
+//             if(map.containsKey(rem)){
+//                 int index = map.get(rem);
+//                 if(index == j)continue; // yaad rakhle bhai isko
+//                 // same element twice condition
+//                 return new int[]{j,index};
+//             }
+            
+//         }
+//         return new int[]{};//empty array for just sake of question
+//     }
+// }
+
+
+
+class Solution{
     public int[] twoSum(int[] nums, int target) {
-        
-        HashMap<Integer , Integer> map = new HashMap();
-        
-        //Fill HM
-        for(int i = 0; i<nums.length;  i++){
+        Map<Integer , Integer> map = new HashMap<>();
+        for(int i =0; i<nums.length;i++){
             map.put(nums[i],i);
         }
-        
-        //Searching
-        for(int i = 0; i<nums.length; i++){
-            int num = nums[i];
-            int rem = target - num;
+        for(int j=0;j<nums.length;j++){
+            int val = nums[j];
+            int rem = target-val;
+            
             if(map.containsKey(rem)){
                 int index = map.get(rem);
-                if(index == i)continue; 
-                // same element twice condition
-                return new int[]{i,index};
+                if(index == j)continue;
+                return new int[]{j , index};
             }
         }
         return new int[]{};
