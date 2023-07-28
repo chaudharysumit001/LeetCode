a5623d238c19f6427c671cbc29c59aaf62ee11bf
Chaudhary Sumit <86030135+chaudharysumit001@users.noreply.github.com>
2023-07-28 16:50:04 +0530

    Time: 0 ms (100.00%), Space: 44.1 MB (88.83%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.java
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.java
@@ -1,3 +1,22 @@
+// class Solution {
+//     public char nextGreatestLetter(char[] letters, char target) {
+//         int start = 0;
+//         int end = letters.length-1;
+        
+//         while(start< end){
+//             int mid = start+(end-start)/2;
+            
+//             if(letters[mid]<start){
+//                 start = mid+1;
+//             }
+//             else{
+//                 end = mid-1;
+//             }
+//         }
+//         return letters[start% letters.length]
+//     }
+// }
+
 class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
         int start = 0;
