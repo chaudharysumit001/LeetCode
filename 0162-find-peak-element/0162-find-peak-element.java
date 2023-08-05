c76f53985ca2d74dab21c6e55786465c25e8eb3b
Chaudhary Sumit <86030135+chaudharysumit001@users.noreply.github.com>
2023-08-05 09:58:44 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (88.51%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,16 +1,34 @@
+// class Solution {
+//   public int findPeakElement(int[] nums) {
+//     int l = 0;
+//     int r = nums.length - 1;
+
+//     while (l < r) {
+//       final int m = (l + r) / 2;
+//       if (nums[m] >= nums[m + 1])
+//         r = m;
+//       else
+//         l = m + 1;
+//     }
+
+//     return l;
+//   }
+// }
+
 class Solution {
-  public int findPeakElement(int[] nums) {
-    int l = 0;
-    int r = nums.length - 1;
-
-    while (l < r) {
-      final int m = (l + r) / 2;
-      if (nums[m] >= nums[m + 1])
-        r = m;
-      else
-        l = m + 1;
+    public int findPeakElement(int[] arr) {
+        int start = 0;
+        int end = arr.length-1;
+        while(start<end){
+            int mid = start+(end-start)/2;
+            
+            if(arr[mid]<arr[mid+1]){
+                start = mid+1;
+            }
+            else{
+                end = mid;
+            }
+        }
+        return start;
     }
-
-    return l;
-  }
 }
