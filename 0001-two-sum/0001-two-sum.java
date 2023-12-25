cc7c26020bbbf389b1067a9c04b344d288d23552
Chaudhary Sumit <86030135+chaudharysumit001@users.noreply.github.com>
2023-12-25 11:27:04 +0530

    Time: 4 ms (63.48%), Space: 44.9 MB (5.04%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,22 +1,20 @@
-
- class Solution {
-     public int[] twoSum(int[] nums, int target){
-         Map<Integer, Integer> map = new HashMap <>(); 
-         for(int i = 0; i<nums.length;  i++){
-             map.put(nums[i],i);
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer , Integer> map = new HashMap <>();
+        for(int i = 0; i<nums.length;  i++){
+            map.put(nums[i],i);
          }
-         for(int i =0; i<nums.length;i++){
-             int val = nums[i];
-             int rem = target - val;
-             if(map.containsKey(rem)){
-                 int index = map.get(rem);
-                 if(index == i){
-                     continue;
-                 }
-                return new int[]{i,index};
-                 
-             }
-         }
-     return new int[]{};
-     }
- }
+        for(int j =0;j<nums.length;j++){
+            int val = nums[j];
+            int rem = target - val;
+            if(map.containsKey(rem)){
+                int index = map.get(rem);
+                if(index == j)continue; // yaad rakhle bhai isko
+                // same element twice condition
+                return new int[]{j,index};
+            }
+            
+        }
+        return new int[]{};//empty array for just sake of question
+    }
+}
