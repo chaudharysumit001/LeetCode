cb7bd028b6c61cb2c145b8e8a9544790dafe1425
Chaudhary Sumit <86030135+chaudharysumit001@users.noreply.github.com>
2023-12-25 11:26:43 +0530

    Time: 5 ms (57.40%), Space: 44.6 MB (5.04%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,42 +1,22 @@
-// class Solution {
-//     public int[] twoSum(int[] nums, int target) {
-//         Map<Integer , Integer> map = new HashMap <>();
-//         for(int i = 0; i<nums.length;  i++){
-//             map.put(nums[i],i);
-//          }
-//         for(int j =0;j<nums.length;j++){
-//             int val = nums[j];
-//             int rem = target - val;
-//             if(map.containsKey(rem)){
-//                 int index = map.get(rem);
-//                 if(index == j)continue; // yaad rakhle bhai isko
-//                 // same element twice condition
-//                 return new int[]{j,index};
-//             }
-            
-//         }
-//         return new int[]{};//empty array for just sake of question
-//     }
-// }
 
-
-
-class Solution{
-    public int[] twoSum(int[] nums, int target) {
-        Map<Integer , Integer> map = new HashMap<>();
-        for(int i =0; i<nums.length;i++){
-            map.put(nums[i],i);
-        }
-        for(int j=0;j<nums.length;j++){
-            int val = nums[j];
-            int rem = target-val;
-            
-            if(map.containsKey(rem)){
-                int index = map.get(rem);
-                if(index == j)continue;
-                return new int[]{j , index};
-            }
-        }
-        return new int[]{};
-    }
-}
+ class Solution {
+     public int[] twoSum(int[] nums, int target){
+         Map<Integer, Integer> map = new HashMap <>(); 
+         for(int i = 0; i<nums.length;  i++){
+             map.put(nums[i],i);
+         }
+         for(int i =0; i<nums.length;i++){
+             int val = nums[i];
+             int rem = target - val;
+             if(map.containsKey(rem)){
+                 int index = map.get(rem);
+                 if(index == i){
+                     continue;
+                 }
+                return new int[]{i,index};
+                 
+             }
+         }
+     return new int[]{};
+     }
+ }
