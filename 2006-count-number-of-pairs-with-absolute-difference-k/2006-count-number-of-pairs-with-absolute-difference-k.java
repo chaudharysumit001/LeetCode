94b41a62ff06c97ea2ba6dbfc72529fb2560825a
Chaudhary Sumit <86030135+chaudharysumit001@users.noreply.github.com>
2023-12-27 07:52:58 +0530

    Time: 4 ms (93.31%), Space: 44.3 MB (5.05%) - LeetHub

--- 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.java
+++ 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.java
@@ -1,42 +1,66 @@
 // class Solution {
 //     public int countKDifference(int[] nums, int k) {
-//         int count = 0; // Initialize the count of pairs with a difference of k
-//         Map<Integer, Integer> m = new HashMap<>(); // Create a HashMap to store the frequency of each number in the array
+//         Map <Integer, Integer> map = new HashMap <> ();
+//         int count = 0;
         
-//         // Iterate over the array of numbers
-//         for(int i = 0; i < nums.length; i++) {
-//             // Check if there is a number in the HashMap whose difference with nums[i] is k
-//             if(m.containsKey(nums[i] - k)) {
-//                 // If so, increment the count by the frequency of that number in the HashMap
-//                 count += m.get(nums[i] - k);
+//         for(int i =0; i<nums.length; i++){
+//             if(map.containsKey(nums[i]-k)){
+//                 count +=  map.get(nums[i] - k);
 //             }
-//             // Check if there is a number in the HashMap whose sum with nums[i] is k
-//             if(m.containsKey(nums[i] + k)) {
-//                 // If so, increment the count by the frequency of that number in the HashMap
-//                 count += m.get(nums[i] + k);
+//             if(map.containsKey(nums[i]+k)){
+//                 count +=  map.get(nums[i] + k);
 //             }
-//             // Add the current number to the HashMap or increment its frequency if it is already present
-//             m.put(nums[i], m.getOrDefault(nums[i], 0) + 1);   
+//             else{
+//                 map.put(nums[i], map.getOrDefault(nums[i] , 0)+1);
+//             }
 //         }
-
-//         // Return the final count of pairs with a difference of k
-//         return count;  
+//         return count;
+        
 //     }
 // }
 class Solution {
     public int countKDifference(int[] nums, int k) {
-        int sum =0;
-        int count =0;
-        for(int i =0; i<nums.length;i++){
-            for(int j =i+1; j<nums.length;j++){
-                sum = Math.abs(nums[i]-nums[j]);
-                if(sum == k){
-                    count++;
-                    
-                    sum = 0;
-                }
-            }
-        }
+        
+       HashMap<Integer,Integer> map = new HashMap<>();
+        
+       int n = nums.length;
+        
+        int count = 0;
+        
+       for(int x : nums){
+                   
+           // check for k + x in the map
+           if(map.containsKey(k + x)){
+               count += map.get(k + x);
+           }
+           
+           // check for x - k in the map
+           if(map.containsKey(x - k)){
+               count += map.get(x - k);
+           }
+           
+          // Increase the frequency of the element in the map 
+          map.put(x,map.getOrDefault(x,0)+1);           
+       }
+        
         return count;
     }
 }
+
+// class Solution {
+//     public int countKDifference(int[] nums, int k) {
+//         int sum =0;
+//         int count =0;
+//         for(int i =0; i<nums.length;i++){
+//             for(int j =i+1; j<nums.length;j++){
+//                 sum = Math.abs(nums[i]-nums[j]);
+//                 if(sum == k){
+//                     count++;
+                    
+//                     sum = 0;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// }
